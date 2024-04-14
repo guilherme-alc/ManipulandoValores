a8b8dd028d9e676e8c95831f1e640eb2529b2100
guilherme-alc <guilhermealc01@gmail.com>
2024-04-14 13:57:20 -0300

    Adicionando exemplo do uso de tupla

---
+++ Tupla/Models/LeituraArquivo.cs
@@ -0,0 +1,21 @@
+﻿namespace Tupla.Models
+{
+    internal class LeituraArquivo
+    {
+        //Tupla em método
+        public static (bool Sucesso, string[] Linhas, int QuantidadeLinhas) LerArquivo(string caminho)
+        {
+            try
+            {
+                string[] linhas = File.ReadAllLines(caminho);
+                //Caso consiga ler o arquivo, retorna true, as linhas do arquivo e a quantidade de linhas
+                return (true, linhas, linhas.Length);
+            } 
+            catch (Exception)
+            {
+                //Caso não consiga, retorna false, lista de string vazia, 0 quantidade de linhas
+                return (false, new string[0], 0);
+            }
+        }
+    }
+}
---
+++ Tupla/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tupla.Models;
+
+namespace Tupla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Formas de criação de tupla
+            (int Id, string Nome, char Sexo) tuplaPessoa = (25987, "Guilherme Augusto", 'M');
+            //Dessas formas não é possível nomear os dados:
+            ValueTuple<int, string, decimal> tuplaPessoa1 = (25698, "Eric", 'M');
+            var tuplaPessoa2 = Tuple.Create(26597, "Maite", 'F');
+            
+            
+            Console.WriteLine($"Id: {tuplaPessoa.Id}");
+            Console.WriteLine($"Nome: {tuplaPessoa.Nome}");
+            Console.WriteLine($"Sexo: {tuplaPessoa.Sexo}");
+            Console.WriteLine();
+            Console.WriteLine($"Id: {tuplaPessoa2.Item1}");
+            Console.WriteLine($"Nome: {tuplaPessoa2.Item2}");
+            Console.WriteLine($"Sexo: {tuplaPessoa2.Item3}");
+            Console.WriteLine();
+
+            //var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+            (bool sucesso, string [] linhasArquivo, _) = LeituraArquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+
+            if(sucesso)
+            {
+                //Console.WriteLine($"Quantidade de linhas do arquivo {quantidadeLinhas}");
+                foreach(string linha in linhasArquivo)
+                {
+                    Console.WriteLine(linha);
+                }
+            } else
+            {
+                Console.WriteLine("Não foi possível ler o arquivo");
+            }
+        }
+    }
+}
